435cc08b9524f9fbde9dcb3e142c6b2404f1c4a1
Dushan Perera <dushan.v.perera@gmail.com>
2016-08-23 11:19:19 -0700

    added index route

--- client/src/app/index.jsx
+++ client/src/app/index.jsx
@@ -8,11 +8,12 @@ import DetailsPage from 'content/details/details.jsx';
 
 import bootstrap from '../public/css/bootstrap.min.css';
 import Main from 'common/main/main.jsx';
-import {Router, Route, hashHistory} from 'react-router';
+import {Router, Route, hashHistory, IndexRoute} from 'react-router';
 
 ReactDom.render(
   (<Router history={hashHistory}>
     <Route path="/" component={Main}>
+      <IndexRoute component={RatingsPage}/>
       <Route path="top-rated" component={RatingPage}/>
       <Route path="comedy" component={ComedyPage}/>
       <Route path="drama" component={DramaPage}/>
